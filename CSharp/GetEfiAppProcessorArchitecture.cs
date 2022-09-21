3ed8436c86388d77e00b4b3d2f28555eefef0663
Tim4ik <gutii.ip.pl@gmail.com>
2022-09-21 20:35:04 +0400

    Create GetEfiAppProcessorArchitecture.cs
    With this code, in C# you can get architecture of EFI Application indicated at "PathToEfiApp" string.

---
+++ CSharp/GetEfiAppProcessorArchitecture.cs
@@ -0,0 +1,27 @@
+namespace ExampleApp
+{
+    class ExampleClass
+    {
+        public static void Main()
+        {
+            string[] Arch;
+            string PathToEfiApp = null;
+            
+            try
+            {
+                byte[] data = File.ReadAllBytes(PathToEfiApp);
+                switch (BitConverter.ToUInt16(data, BitConverter.ToInt32(data, 0x3c) + 4))
+                {
+                    case 0x8664: Arch = new string[] { "x64", "AMD x64 Processor" }; break;
+                    case 0x0200: Arch = new string[] { "x64", "Intel x64 Processor" }; break;
+                    case 0x014c: Arch = new string[] { "ia32", "Intel x32 or i386 Processor" }; break;
+                    case 0xaa64: Arch = new string[] { "aa64", "Advanced RISC Machine x64 Processor" }; break;
+                }
+            }
+            catch { }
+            
+            Console.WriteLine("EFI application architecture : {0},", Arch[0]);
+            Console.WriteLine(arch[1]);
+        }
+    }
+}
